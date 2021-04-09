4129201ba89639c020357e64e100b0c2274f8e90
vitalii <tregubovvakharkov@gmail.com>
2021-04-09 23:48:25 +0300

    styles adjustment

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -2,19 +2,21 @@
   <header class="page-header flex align-center between">
     <router-link to="/" class="logo"><h1>InterStore</h1></router-link>
     <section class="flex align-center">
-      <div class="flex align-center mode" title="dark-mode">
-        <input type="checkbox" id="mode" :checked="theme === 'dark'" class="mode-input" @change="toggleMode" />
-        <label for="mode" class="mode-label border-rounded"></label>
-      </div>
-      <a v-if="!userLoggedIn" href="#" @click.prevent="toggleModal" class="auth border-rounded">Log in / Sign up</a>
-      <template v-else>
-        <a href="#" @click.prevent="signOut" class="menu-link logout" title="sign out">
-          <i class="fal fa-sign-out"></i>
-        </a>
-        <router-link :to="{ name: 'Cart' }" class="menu-link cart" :data-quantity="chosenProducts.length" title="cart">
+      <div v-if="userLoggedIn" class="flex align-center relative">
+        <button @click.stop="accountMenuShown = !accountMenuShown" data-account-menu="sdf" class="account-menu-btn">
+          <i class="fad fa-user"></i>
+        </button>
+        <router-link :to="{ name: 'Cart' }" class="cart-link" :data-quantity="chosenProducts.length" title="cart">
           <i class="fal fa-shopping-cart"></i>
-        </router-link>    
-      </template>
+        </router-link>
+        <div :class="[{ shown: accountMenuShown}, { dark: theme  === 'dark' }, 'account-settings-menu']" data-account-menu="sfd">
+          <input type="checkbox" id="mode" :checked="theme === 'dark'" class="mode-input" @change="toggleMode" />
+          <label for="mode" class="mode-label">Dark mode</label>
+          <a href="#" @click.prevent="signOut" class="logout" title="sign out">Log out</a>
+        </div>
+      </div>
+
+      <a v-else href="#" @click.prevent="toggleModal" class="auth border-rounded">Log in / Sign up</a>
     </section>
   </header>
 </template>
@@ -26,7 +28,8 @@ export default {
   name: 'Header',
   data() {
     return {
-      theme: ''
+      theme: '',
+      accountMenuShown: false
     }
   },
   methods: {
@@ -43,12 +46,23 @@ export default {
       window.document.body.setAttribute('data-mode', newTheme)
       this.theme = newTheme
     },
+    openAccountMenu() {
+
+    }
   },
   created() {
     !localStorage.getItem('theme') && localStorage.setItem('theme', 'light')
     const theme = localStorage.getItem('theme')
     window.document.body.setAttribute('data-mode', theme)
     this.theme = theme
+
+    window.document.body.addEventListener('click', (e) => {
+      e.stopPropagation()
+      if (!e.target.dataset.accountMenu && !e.target.parentElement.dataset.accountMenu) {
+        this.accountMenuShown = false
+        console.log('triggered')
+      }
+    })
   },
   computed: {
     ...mapState(['userLoggedIn', 'chosenProducts'])
@@ -72,34 +86,120 @@ export default {
   height: 60px
   background-color: #fff
 
-  .mode
-    margin-top: 8px
+  .account-menu-btn
+    width: 36px
+    height: 36px
+    border-radius: 50%
+    color: inherit
+    line-height: 36px
+    font-size: 1.2rem
+    border: 2px solid
+
+    &:focus,
+    &:hover
+      outline: none
+      color: $color-active
+
+  .account-settings-menu
+    position: absolute
+    z-index: 100
+    display: none
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.3)
+    background-color: #fff
+    right: 0
+    top: 50px
+    height: 90px
+    width: 200px
+    padding: 15px 0
+
+    .logout
+      width: 100%
+      height: 30px
+      line-height: 30px
+      padding-left: 20px
+      
+
+      &:hover
+        color: $color-active
+        background-color: rgba(0, 0, 0, 0.1)
+
+    &.dark
+      background-color: #333
+
+      .logout:hover,
+      .mode-label:hover
+        background-color: rgba(255, 255, 255, 0.1)
+
+    &.shown
+      display: flex
+      flex-direction: column
+      justify-content: space-between
+      align-items: flex-end
 
     .mode-label
-      width: 40px
-      height: 20px
-      background-color: lightgrey
-      margin-right: 10px
+      position: relative
+      padding-left: 20px
+      width: 100%
+      height: 30px
+      line-height: 30px
 
-      &::before
+      &::before,
+      &::after
         @include pseudoEl
-        left: 0
-        bottom: 0
+        background-color: #ddd
+        width: 40px
+        height: 20px
+        border-radius: 25px
+        right: 20px
+        top: 4px
+
+      &::after
+        right: 40px
         width: 20px
         height: 20px
         border-radius: 50%
         background-color: #888
         transition: all .3s ease
 
+      &:hover
+        color: $color-active
+        background-color: rgba(0, 0, 0, 0.05)
+
     .mode-input
       display: none
 
-      &:checked ~ .mode-label
+      &:checked ~ .mode-label::before
         background-color: $color-active
 
-        &::before
-          transform: translateX(20px)
-          background-color: #fff 
+      &:checked ~ .mode-label::after
+        transform: translateX(20px)
+        background-color: #fff
+    
+    // .mode-label
+    //   display: inline-block
+    //   width: 40px
+    //   height: 20px
+    //   background-color: lightgrey
+    //   margin-right: 20px
+
+    //   &::before
+    //     @include pseudoEl
+    //     left: 0
+    //     bottom: 0
+    //     width: 20px
+    //     height: 20px
+    //     border-radius: 50%
+    //     background-color: #888
+    //     transition: all .3s ease
+
+    
+
+    //   &:checked ~ .mode-label
+    //     background-color: $color-active
+
+    //     &::before
+    //       transform: translateX(20px)
+    //       background-color: #fff 
 
   .auth
     color: #fff
@@ -108,34 +208,27 @@ export default {
     letter-spacing: .05rem
     font-size: 1.2rem
 
-  .menu-link
-    margin-left: 20px
-    width: 40px
-    height: 40px
-    text-align: center
+  .cart-link
+    position: relative
+    margin-left: 30px
+    margin-right: 10px
     line-height: 40px
+    font-size: 2rem 
 
     &:hover
       color: $color-active
-
-    i
-      vertical-align: middle
-      font-size: 1.6rem  
-
-    &.cart
-      position: relative
     
-      &::before
-        @include pseudoEl
-        content: attr(data-quantity)
-        top: -5px
-        left: 20px
-        font-size: 0.8rem
-        width: 20px
-        height: 20px
-        line-height: 20px
-        border-radius: 50%
-        color: #fff
-        background-color: $color-active
-        text-align: center
+    &::before
+      @include pseudoEl
+      content: attr(data-quantity)
+      top: -5px
+      left: 20px
+      font-size: 0.8rem
+      width: 20px
+      height: 20px
+      line-height: 20px
+      border-radius: 50%
+      color: #fff
+      background-color: $color-active
+      text-align: center
 </style>
